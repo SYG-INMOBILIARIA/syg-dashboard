96e55d8ba81cd9aaae7cc64fcaf0dc1a36bf66db
william calle <william.usp@hotmail.com>
2025-08-04 12:28:26 -0500

    fix: update mapbox token

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environments = {
   baseUrl: 'http://localhost:3000/api/v1',
   webSocketBookingsUrl: 'http://localhost:3000',
   defaultImgUrl: 'assets/img/Image-not-found.png',
-  mapbox_key: "pk.eyJ1Ijoid2lsbGlhbWl0eiIsImEiOiJjbTN5ajBjMDcxMm0xMmpteDJpOTZmY3pjIn0.w_MsoDWmVsITmGYVceqyOg"
+  mapbox_key: "pk.eyJ1IjoiZ2pob2FuZmVyIiwiYSI6ImNtNHVmc3ZpajBqaXQyam9tanFucXY2cWMifQ.iO0GZnt3Cf8zaDs_uNaZ1w"
 };
