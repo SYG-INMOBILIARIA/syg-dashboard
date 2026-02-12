26ba3e31628c9331665a1ec2a6e53929f7cf6896
William Calle <william.calle@canvasandcloud.pe>
2026-02-12 16:21:14 -0500

    fix: add background login page

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
-  styles: ``
+  styles: `
+
+    .bg-syg-fondo {
+      background-image: url('/assets/img/wallpaper.jpeg');
+      background-size: cover;
+      background-position: center;
+    }
+
+  `
 })
 export class AuthLayoutComponent {
 
