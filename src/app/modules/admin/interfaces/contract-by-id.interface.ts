d71a10e6303d2e04b026000833c203c16cc95c76
William Calle <william.calle@canvasandcloud.pe>
2026-04-10 12:54:36 -0500

    fix: show detail initial-pay info

--- src/app/modules/admin/interfaces/contract-by-id.interface.ts
+++ src/app/modules/admin/interfaces/contract-by-id.interface.ts
@@ -1,32 +1,40 @@
 import { User } from "@modules/security/interfaces";
-import { Client, Financing, Lote, Proyect } from ".";
+import { Client, Financing, Lote, PaymentMethod, Proyect } from ".";
 import { PaymentType } from "../enum";
+import { Photo } from "@shared/interfaces";
 
 export interface ContractByID {
-  isActive:           boolean;
-  userCreate:         string;
-  createAt:           Date;
-  id:                 string;
-  code:               string;
-  year:               number;
-  paymentType:        PaymentType;
-  documentation:      string;
-  observation:        string;
-  contractSatus:      string;
-  selledUserId:       string;
-  loteAmount:         number;
-  quotesAmount:       number;
-  initialAmount:      number;
-  amountToFinancing:  number;
-  numberOfQuotes:     number;
-  interestPercent:    number;
-  interestAmount:     number;
-  anulateObservation: null;
-  anulateAt:          null;
-  userAnulate:        null;
-  lotes:              Lote[];
-  clients:            Client[];
-  proyect:            Proyect;
-  financing:          Financing;
-  seller:             User;
+  isActive:             boolean;
+  userCreate:           string;
+  createAt:             Date;
+  id:                   string;
+  code:                 string;
+  year:                 number;
+  paymentType:          PaymentType;
+  documentation:        string;
+  observation:          string;
+  contractSatus:        string;
+  selledUserId:         string;
+  loteAmount:           number;
+  quotesAmount:         number;
+  initialAmount:        number;
+  amountToFinancing:    number;
+  numberOfQuotes:       number;
+  interestPercent:      number;
+  interestAmount:       number;
+  anulateObservation:   null;
+  anulateAt:            null;
+  userAnulate:          null;
+  lotes:                Lote[];
+  clients:              Client[];
+  proyect:              Proyect;
+  financing:            Financing;
+  seller:               User;
+
+  initialPaidDate:      Date;
+  observationInitial:   string;
+  operationCodeInitial: string;
+  voucherInitial:       Photo;
+  paymentMethodInitial: PaymentMethod
+
 }
